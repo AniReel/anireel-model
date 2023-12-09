aa13d84e8556e2c7fa0bbb053e1b1a56c6416545
elias-dzobo <elias.dzobo@gmail.com>
2023-12-09 13:36:09 +0000

    mini dataset: done

--- v0.py
+++ v0.py
@@ -1,7 +1,9 @@
 import requests
 import pathlib 
+import pandas as pd 
 
 id = 100
+n = 10
 
 url = f"https://api.jikan.moe/v4/anime/{id}/full"
 
@@ -11,24 +13,32 @@ response = requests.get(url).json()
 
 anime = []
 
-res_dict = {}
+for i in range(1, n):
 
-print(response['data']['mal_id'])
-print(response['data']['url'])
-print(response['data']['trailer']['url'])
-print(response['data']['title'])
-print(response['data']['episodes'])
-print(response['data']['status'])
-print(response['data']['airing'])
-print(response['data']['duration'])
-print(response['data']['rating'])
-print(response['data']['score'])
-print(response['data']['rank'])
-print(response['data']['popularity'])
-print(response['data']['synopsis'])
-print(response['data']['year'])
-print(response['data']['genres'])
-print(response['data']['themes'])
-print(response['data']['streaming'])
+    res_dict = {}
+
+    res_dict['id'] = response['data']['mal_id']
+    res_dict['url'] = response['data']['url']
+    res_dict['trailer'] = response['data']['trailer']['url']
+    res_dict['title'] = response['data']['title']
+    res_dict['number_of_episodes'] = response['data']['episodes']
+    res_dict['status'] = response['data']['status']
+    res_dict['is_airing'] = response['data']['airing']
+    res_dict['duration_per_episode'] = response['data']['duration']
+    res_dict['rating'] = response['data']['rating']
+    res_dict['score'] = response['data']['score']
+    res_dict['rank'] = response['data']['rank']
+    res_dict['popularity'] = response['data']['popularity']
+    res_dict['description'] = response['data']['synopsis']
+    res_dict[''] = response['data']['year']
+    res_dict['genres'] = response['data']['genres']
+    res_dict['themes'] = response['data']['themes']
+    res_dict['streaming_sites'] = response['data']['streaming']
+
+    anime.append(res_dict)
+
+anime_df = pd.DataFrame(anime)
+
+anime_df.to_csv('mini_data.csv') 
 
 #res_dict['id'] = response[]

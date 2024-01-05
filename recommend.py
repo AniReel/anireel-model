6fabdf5356faf1b5b048d76acc92839831faad69
elias-dzobo <elias.dzobo@gmail.com>
2024-01-05 10:56:34 +0000

    test working, endpoints and todos created

--- recommend.py
+++ recommend.py
@@ -10,7 +10,14 @@ config = dotenv_values(env_name)
 
 co = cohere.Client(os.environ['cohere_api'])
 
+pinecone.init(
+    api_key= config['pinecone_api'],
+    environment= config['pinecone_env']
+)
+
 index = pinecone.Index(config['pinecone_index'])
+
+
 
 def embed_text(text):
     embeddings = co.embed(
@@ -36,9 +43,18 @@ def vector_search(desc):
     res = index.query([embeddings.embeddings[0]], top_k=2, include_metadata=True)
 
     for match in res['matches']:
-        if match['score'] < 0.8:
-            continue 
-
         results.append(match['metadata']['text'])
 
     return results
+
+def get_anime(res):
+    data = pd.read_csv('mini_data.csv')
+
+    df = pd.DataFrame()
+
+    for desc in res:
+        anime = data[data['description'] == desc]
+
+        df = pd.concat([df, anime])
+
+    return df 
